c6f1db3d38575aaf9349126e71b54b1942fb0480
ink0rr <tomflynnbeltsazar@gmail.com>
2024-11-15 18:43:56 +0700

    Update config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,4 @@
-Clear-Host
-
-if ($env:TERM_PROGRAM -eq 'vscode') {
-    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/avit.omp.json" | Invoke-Expression
-}
-else {
-    $PROFILE_DIR = Split-Path $PROFILE -Parent
-    oh-my-posh init pwsh --config "$PROFILE_DIR/theme.omp.json" | Invoke-Expression
-}
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/avit.omp.json" | Invoke-Expression
 
 function su { 
     Start-Process wt -ArgumentList "nt -d ." -Verb RunAs
